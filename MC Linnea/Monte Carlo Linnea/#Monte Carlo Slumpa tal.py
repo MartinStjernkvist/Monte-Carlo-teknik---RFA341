98fe5a8dbd6b3f3de9b0e699b464602ce4f0fc94
RainRe1 <linneato02@gmail.com>
2024-12-20 11:54:56 +0100

    Lyckades slumpa energierna på fotoner

--- MC Linnea/Monte Carlo Linnea/#Monte Carlo Slumpa tal.py
+++ MC Linnea/Monte Carlo Linnea/#Monte Carlo Slumpa tal.py
@@ -1,7 +1,8 @@
 #Monte Carlo Slumpa tal
 import random as rand
-rand.random()# ger slumptal mellan 0 till 1
+rand.random() # ger slumptal mellan 0 till 1
 import numpy as np
+np.random.rand( )# ger slumptal mellan 0 till 1)
 import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
 #Ursprungsenergier från nuklider
@@ -12,20 +13,39 @@ At211_energ=[5869, 5211.9,5140.3, 4993.4,4895.4] #keV
 Lu177_intens=[10.38, 6.2, 0.216, 0.2012, 0.1726, 0.047]
 At211_intens=[41.78, 0.0039, 0.0011, 0.0004, 0.00004]
 
-#Gör om till tal och tar totala sannolikheten för varje energi från en tom matris
+#Gör om intensiteten till tal och tar totala sannolikheten för varje energi från en tom matris
 Lu177_sannolik=np.zeros(len(Lu177_intens))
 At211_sannolik=np.zeros(len(At211_intens))
 
 for i in range(len(Lu177_intens)):
     Lu177_intens[i]=Lu177_intens[i]*10**-2
-    At211_intens[i]=At211_intens[i]*10**-2
 
 
 for i in range(len(Lu177_intens)):
     if i==0:
         Lu177_sannolik[i]=Lu177_intens[i]
-        At211_sannolik[i]=At211_intens[i]
+
     else:
         Lu177_sannolik[i]= Lu177_intens[i]+Lu177_intens[i-1]
-        At211_sannolik[i]= At211_intens[i]+At211_intens[i-1]
-#Hitta ett sätt att slumpa ursprungenergin
+
+#Tar ett slumpmässigt värde och genererar antalet
+Foton_energi=[]
+Antal_iterationer=100
+for i in range(Antal_iterationer):
+    Slump_tal=np.random.rand()
+    if Slump_tal<=Lu177_sannolik[0]:
+        Foton_energi.append(Lu177_energ[0]) 
+    elif Slump_tal<=Lu177_sannolik[1]:
+        Foton_energi.append(Lu177_energ[1]) 
+    elif Slump_tal<=Lu177_sannolik[2]:
+        Foton_energi.append(Lu177_energ[2])
+    elif Slump_tal<=Lu177_sannolik[3]:
+        Foton_energi.append(Lu177_energ[3]) 
+    elif Slump_tal<=Lu177_sannolik[4]:
+        Foton_energi.append(Lu177_energ[4]) 
+    elif Slump_tal<=Lu177_sannolik[5]:
+        Foton_energi.append(Lu177_energ[5]) 
+    else:
+        continue
+
+print(Foton_energi)
