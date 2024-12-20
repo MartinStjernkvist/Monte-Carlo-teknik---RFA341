c34d4a45e4c9f7dcc5854212de770823a2f86d69
RainRe1 <linneato02@gmail.com>
2024-12-20 19:01:41 +0100

    Add files via upload

--- MC Linnea/Monte Carlo Linnea/#Startposition lila och stora tumören.py
+++ MC Linnea/Monte Carlo Linnea/#Startposition lila och stora tumören.py
@@ -1,37 +1,10 @@
-#Monte Carlo Slumpa tal
+#Startposition lila och stora tumören
+
 import random as rand
 rand.random() # ger slumptal mellan 0 till 1
 import numpy as np
 np.random.rand( )# ger slumptal mellan 0 till 1)
 import math as ma
-
-#Ursprungsenergier från nukliden
-Y90_energ=2278.7 #keV men ska slumpa i spektrumet i en av filerna
-
-
-#Steglängden på fotonerna
-
-#Ta reda på majortiteta attenuerigskoefficienten för varje matris/slice
-file_path_attenuering="C:\Users\Admin\Documents\GitHub\Monte Carlo Linnea\Attenueringsdata.xlsx"
-... #ta reda på my_m genom att ta max för allavoxlar attenueringskoefficient
-my_m=np.max()
-
-#Steglängden på fotonerna
-steg=-ma.log(rand.random())/my_m
-
-
-
-#Hittar filen 
-#file_path_betasönderfall="C:\Users\Admin\Documents\GitHub\Monte Carlo Linnea\Y90_Spektrum.xlsx"
-
-#Läser en Excel fil
-import pandas as pd
-df=pd.read_excel(r"C:\Users\Admin\Documents\GitHub\Monte Carlo Linnea\Y90_Spektrum.xlsx" )
-print(df)
-#Lägg till sannolikheten för beta sönderfall? på något sätt hitta hur man gör det i en excel fil
-
-
-
 #Slumpa startpositionen för alfa och beta
 #Antar att tumören är i mitten av koordinatsystemet
 
@@ -65,4 +38,3 @@ for i in range(Antal_iterationer):
         Startpos_yta.append([x,y,z])
     else:
         continue
-
