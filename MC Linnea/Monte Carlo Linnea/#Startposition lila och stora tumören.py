3975f83da40803854d1d46858436e30283b52f32
RainRe1 <linneato02@gmail.com>
2024-12-22 10:30:11 +0100

    Testade slumpa position
    Men måste bestämma lämpliga intervall för x,y,z koordinaten för att få en slumpmässig position i tumören och på ytan av tumören

--- MC Linnea/Monte Carlo Linnea/#Startposition lila och stora tumören.py
+++ MC Linnea/Monte Carlo Linnea/#Startposition lila och stora tumören.py
@@ -11,30 +11,32 @@ import math as ma
 #Position på sfärens yta för lilla tumören
 radie_liten=300*10**-6 # i meter
 Sfäryta_liten=4*radie_liten**2*ma.pi
-print(Sfäryta_liten)
 
-Antal_iterationer=100
+
+Antal_iterationer=1000
 Startpos_yta=[]
 for i in range(Antal_iterationer):
-    x=np.random.uniform(-0.5,0.5)#Slumpmässig koordinat mellan 1 m i koordinatsystemet
-    y=np.random.uniform(-0.5,0.5)
-    z=np.random.uniform(-0.5,0.5)
+    x=np.random.uniform(-0.5*10**-6,0.5*10**-6)#Slumpmässig koordinat mellan 1 mm i koordinatsystemet
+    y=np.random.uniform(-0.5*10**-6,0.5*10**-6)
+    z=np.random.uniform(-0.5*10**-6,0.5*10**-6)
     if radie_liten**2==y**2+z**2+x**2:
         Startpos_yta.append([x,y,z])
     else:
         continue
+print(Startpos_yta)
 #Position i sfärens volym för stora tumören
 radie_stor=1*10**-3 # i meter
-Sfärvolym_stor=4*radie_liten**3*ma.pi/3
-print(Sfärvolym_stor)
+Sfärvolym_stor=4*radie_stor**3*ma.pi/3
 
-Antal_iterationer=100
+
+Antal_iterationer=1
 Startpos_volym=[]
 for i in range(Antal_iterationer):
-    x=np.random.uniform(-0.5,0.5) #Slumpmässig koordinat mellan 1 m i koordinatsystemet
-    y=np.random.uniform(-0.5,0.5)
-    z=np.random.uniform(-0.5,0.5)
+    x=np.random.uniform(-0.5*10**-3,0.5*10**-3) #Slumpmässig koordinat mellan 1 mm i koordinatsystemet
+    y=np.random.uniform(-0.5*10**-3,0.5*10**-3)
+    z=np.random.uniform(-0.5*10**-3,0.5*10**-3)
     if radie_stor**2>=y**2+z**2+x**2:
         Startpos_volym.append([x,y,z])
     else:
         continue
+#print(Startpos_volym)
