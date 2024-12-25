067ff29672203148b42f2281976ed0af90e57bed
MartinStjernkvist <mr.martin.pro@gmail.com>
2024-12-25 04:48:44 +0100

    .

--- Martin/imports.py
+++ Martin/imports.py
@@ -15,3 +15,50 @@ from numpy import random
 #   ----------------------------------------------------------------------
 
 pi = np.pi
+m_e = 0.511 * 10**6 # eV / c^2
+r_e = np.sqrt(0.07941) # b: re2 = e4/Ee2 ≈ 0.07941 b, https://en.wikipedia.org/wiki/Gamma_ray_cross_section
+a_0 = 5.29177210903 * 10**(-11) * 10**(14) # b, bohr radius of hydrogen
+c = 3 * 10**8
+
+
+
+def plot_stuff(x_data, y_data, scatter, label_data,
+               marker='o', color='blue', x_label='x-label', y_label='y-label', title='1',
+               fig_size=(10, 10), symbol_size=100, font_size=30, alpha=1, line_width=10, x_lim=(0, 0), y_lim=(0, 0),
+               grid=False):
+
+    fig = plt.figure(figsize=fig_size)
+
+    if x_lim != (0, 0) and y_lim != (0, 0):
+        plt.ylim(y_lim)
+        plt.xlim(x_lim)
+
+    if grid == True:
+        plt.grid()
+
+    if scatter == 0:
+        plt.plot(x_data, y_data, color=color, alpha=alpha, linewidth=line_width, label=label_data)
+    elif scatter == 1:
+        plt.scatter(x_data, y_data, marker=marker, color=color, alpha=alpha, s=symbol_size, label=label_data)
+    elif scatter == 2:
+        plt.plot(x_data, y_data, color=color, alpha=alpha, linewidth=line_width, label=label_data)
+        plt.scatter(x_data, y_data, marker=marker, color=color, alpha=alpha, s=symbol_size, label=label_data)
+    else:
+        for i in range(len(x_data)):
+            if scatter[i] == 1:
+                plt.scatter(x_data[i], y_data[i], marker=marker[i], color=color[i],
+                            alpha=alpha, s=symbol_size, label=label_data[i])
+            else:
+                plt.plot(x_data[i], y_data[i], color=color[i], alpha=alpha, linewidth=line_width, label=label_data[i])
+
+    font_size_ticks = font_size * 0.85
+    plt.xlabel(x_label, fontsize=font_size)
+    plt.ylabel(y_label, fontsize=font_size)
+
+    plt.xticks(fontsize=font_size_ticks)
+    plt.yticks(fontsize=font_size_ticks)
+
+    plt.title(title, fontsize=font_size)
+    plt.legend(fontsize=font_size_ticks)
+
+    return fig
--- Martin/sampla_växelverkan.py
+++ Martin/sampla_växelverkan.py
@@ -1,29 +1,67 @@
 from imports import *
 
-def sigma_compton(energi):
-    return print('WIP')
 
-def sigma_foto(energi):
-    return print('WIP')
+class växelverkan:
 
-def sigma_par(energi):
-    return print('WIP')
+    def sigma_compton(self, energi):
+        # klein nishina
+        epsilon = energi / (m_e * c ** 2)
+        sigma_compton = 2 * pi * r_e ** 2 * ((1 + epsilon) / epsilon ** 3 + (
+        (2 * epsilon * (1 + epsilon) / (1 + 2 * epsilon) - np.log(1 + 2 * epsilon))) + np.log(1 + 2 * epsilon) / (
+                                                         2 * epsilon) - (1 + 3 * epsilon) / (1 + 2 * epsilon) ** 2)
+        return sigma_compton
 
-def växelverkan(energi):
-    sigma_compton = sigma_compton(energi)
-    sigma_foto = sigma_foto(energi)
-    sigma_par = sigma_par(energi)
+    def sigma_foto(self, energi):
+        # https://en.wikipedia.org/wiki/Gamma_ray_cross_section
+        # https: // bohr.physics.berkeley.edu / classes / 221 / 1112 / notes / photelec.pdf
+        Z = 82  # räknar inte med Z-beroende, BYT TILL MEDELVÄRDE AV Z FÖR MÄNNISKOKROPP
+        alpha = 1 / 137
+        # return 16 / 3 * np.sqrt(2) * pi * r_e ** 2 * alpha ** 4 * Z ** 5 / energi ** 3.5
+        # return 5 * 10**11 * Z**5 / energi**(3.5)
+        return 16 / 3 * np.sqrt(2) * pi * alpha **8 * a_0**2 * (m_e * c**2)**3.5 * Z**5 / energi**3.5
 
-    Lu177_energ = [sigma_foto, sigma_compton, sigma_par]  # keV
+    def bestäm_växelverkan(self, energi):
+        # energierna på gamma från sönderfallen är för låga för att ge parbildning
+        # -> antingen compton eller foto
+        sigma_foto = växelverkan.sigma_foto(self,energi)
+        sigma_compton = växelverkan.sigma_compton(self, energi)
 
-    # Intensitet i % för energierna
-    Lu177_intens = [10.38, 6.2, 0.216, 0.2012, 0.1726, 0.047]
+        tvärsnitt_lista = [sigma_foto, sigma_compton]  # keV
+        print(tvärsnitt_lista)
 
-    Lu177_sannolik = np.zeros(len(Lu177_intens))
-    for i in range(len(Lu177_intens)):
-        if i == 0:
-            Lu177_sannolik[i] = Lu177_intens[i] / np.sum(Lu177_intens)
+        tvärsnitt_lista_norm = np.zeros(len(tvärsnitt_lista))
+        for i in range(len(tvärsnitt_lista)):
+            if i == 0:
+                tvärsnitt_lista_norm[i] = tvärsnitt_lista[i] / np.sum(tvärsnitt_lista)
+            else:
+                tvärsnitt_lista_norm[i] = tvärsnitt_lista[i] / np.sum(tvärsnitt_lista) + tvärsnitt_lista_norm[i - 1]
+
+        print(tvärsnitt_lista_norm)
+
+        # OBS, kanske måste räkna med rayleigh spridning
+        if np.random.rand() <= tvärsnitt_lista[0]:
+            text = 'foto'
         else:
-            Lu177_sannolik[i] = Lu177_intens[i] / np.sum(Lu177_intens) + Lu177_sannolik[i - 1]
+            text = 'compton'
 
-    rand = np.random.rand()
+        # print(text)
+        return text
+
+iterationer = 5
+for i in range(iterationer):
+    instance = växelverkan().bestäm_växelverkan(1_000_000)
+    if instance == 'foto':
+        print('BINGO')
+
+
+# def plot_stuff(x_data, y_data, scatter, label_data,
+#                marker='o', color='blue', x_label='x-label', y_label='y-label', title='1',
+#                fig_size=(10, 10), symbol_size=100, font_size=30, alpha=1, line_width=10, x_lim=(0, 0), y_lim=(0, 0),
+#                grid=False):
+
+print(16 / 3 * np.sqrt(2) * pi * r_e ** 2 * (1/137) ** 4)
+print(5 * 10**11)
+print(4*10**(1))
+
+print((1.602*10**(-19))**2/(m_e * c**2)*10**(14))
+
