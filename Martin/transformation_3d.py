840dc75c80e466f42f192e65201867a3305e0060
MartinStjernkvist <mr.martin.pro@gmail.com>
2025-01-01 16:45:43 +0100

    .

--- Martin/transformation_3d.py
+++ Martin/transformation_3d.py
@@ -1,7 +1,7 @@
 from imports import *
 
 
-def transform(x_B, y_B, z_B, d_x, d_y, d_z, phi, theta):
+def transform(steg, phi, theta, x_B, y_B, z_B):
     """
     - Börjar på x,y,z - kalla detta koordinatsystem A.
     - Tar ett steg med steglängd s, riktning (phi, theta), enligt koordinatsystemet i A.
@@ -22,6 +22,11 @@ def transform(x_B, y_B, z_B, d_x, d_y, d_z, phi, theta):
     :param phi: vinkel sfärisk
     :param theta: vinkel sfärisk
     """
+
+    d_x = steg * np.sin(theta) * np.cos(phi)
+    d_y = steg * np.sin(theta) * np.sin(phi)
+    d_z = steg * np.cos(theta)
+
 
     enhets_vektorer_A = np.array(
         [
@@ -59,7 +64,7 @@ def transform(x_B, y_B, z_B, d_x, d_y, d_z, phi, theta):
     #         [0, 0, 0, 0]
     #     ])
 
-    angle = pi/2 - theta
+    angle = pi / 2 - theta
     R_y = np.array(
         [
             [np.cos(angle), 0, -np.sin(angle)],
@@ -67,19 +72,17 @@ def transform(x_B, y_B, z_B, d_x, d_y, d_z, phi, theta):
             [np.sin(angle), 0, np.cos(angle)],
         ])
 
-    # tänk att om theta > 0 måste kompenseringen vara rotation med -theta
-    # R_x = np.array(
-    #     [[1,0,0,0], [0, np.cos(theta), -np.sin(theta),0], [0, np.sin(theta), np.cos(theta), 0], [0, 0, 0, 0]])
-
     # R_y = np.identity(3)
     # R_z = np.identity(3)
 
     # R = R_y @ R_z
-    R = R_z @ R_y
-    # R = R_x @ R_z
 
-    print(f'\nR matrix: \n{R}')
-    print(f'\nlength of vector: \nx: {np.linalg.norm(R[0:3,0])}\ny: {np.linalg.norm(R[0:3,1])}\nz: {np.linalg.norm(R[0:3,2])}')
+    # först rotation i theta (y-axeln), sedan rotation i phi (z-axeln)
+    R = R_z @ R_y
+
+    # print(f'\nR matrix: \n{R}')
+    # print(f'\nlength of vector: \nx: {np.linalg.norm(R[0:3,0])}\ny: {np.linalg.norm(R[0:3,1])}\nz: {np.linalg.norm(R[0:3,2])}')
+
     Homogenous_matrix = np.array(
         [
             [R[0, 0], R[0, 1], R[0, 2], d_x],
@@ -87,6 +90,7 @@ def transform(x_B, y_B, z_B, d_x, d_y, d_z, phi, theta):
             [R[2, 0], R[2, 1], R[2, 2], d_z],
             [0, 0, 0, 1]
         ])
+
     result = Homogenous_matrix @ np.array(
         [
             [x_B],
@@ -94,61 +98,64 @@ def transform(x_B, y_B, z_B, d_x, d_y, d_z, phi, theta):
             [z_B],
             [1]
         ])
+
     enhets_vektorer_B = Homogenous_matrix @ enhets_vektorer_A
     return result, enhets_vektorer_B
 
 
-theta = pi / 8
-phi = pi / 8
-x_B = 1
-y_B = 1
-z_B = 1
-s = 3
-d_x = s * np.sin(theta) * np.cos(phi)
-d_y = s * np.sin(theta) * np.sin(phi)
-d_z = s * np.cos(theta)
+if __name__ == "__main__":
+    start = time.time()
 
-result, enhets_vektorer_B = transform(x_B, y_B, z_B, d_x, d_y, d_z, phi, theta)
-x_A = result[0]
-y_A = result[1]
-z_A = result[2]
+    # steg 1: från A till B
+    theta = 3 * pi / 8
+    phi = 3 * pi / 8
+    steg = 3
+    d_x = steg * np.sin(theta) * np.cos(phi)
+    d_y = steg * np.sin(theta) * np.sin(phi)
+    d_z = steg * np.cos(theta)
 
-print(f'\nresult: \n{result}\nenhets_vektorer_B: \n{enhets_vektorer_B}')
+    # steg 2: Från B till C, enligt koordinatsystemet för B
+    x_B = 1
+    y_B = 1
+    z_B = 1
 
+    result, enhets_vektorer_B = transform(steg, phi, theta, x_B, y_B, z_B)
+    x_A = result[0]
+    y_A = result[1]
+    z_A = result[2]
 
-print(f'\ndot product x, y: {np.dot(enhets_vektorer_B[0:3, 0], enhets_vektorer_B[0:3, 1])}')
-print(f'dot product y, z: {np.dot(enhets_vektorer_B[0:3, 1], enhets_vektorer_B[0:3, 2])}')
-print(f'dot product x, z: {np.dot(enhets_vektorer_B[0:3, 0], enhets_vektorer_B[0:3, 2])}')
+    print(f'\nresult: \n{result}\nenhets_vektorer_B: \n{enhets_vektorer_B}')
 
+    print(f'\ndot product x ({enhets_vektorer_B[0:3, 0]}), y ({enhets_vektorer_B[0:3, 1]}): {np.dot(enhets_vektorer_B[0:3, 0], enhets_vektorer_B[0:3, 1])}')
+    print(f'dot product y ({enhets_vektorer_B[0:3, 1]}), z ({enhets_vektorer_B[0:3, 2]}): {np.dot(enhets_vektorer_B[0:3, 1], enhets_vektorer_B[0:3, 2])}')
+    print(f'dot product x ({enhets_vektorer_B[0:3, 0]}), z ({enhets_vektorer_B[0:3, 2]}): {np.dot(enhets_vektorer_B[0:3, 0], enhets_vektorer_B[0:3, 2])}')
 
-fig = plt.figure(figsize=(10, 10))
-ax = plt.axes(projection='3d')
-ax.set_xlim([0, 4])
-ax.set_ylim([0, 4])
-ax.set_zlim([0, 4])
+    fig = plt.figure(figsize=(10, 10))
+    ax = plt.axes(projection='3d')
+    ax.set_xlim([0, 4])
+    ax.set_ylim([0, 4])
+    ax.set_zlim([0, 4])
 
-ax.quiver(0, 0, 0, 1, 0, 0, color='orange')
-ax.quiver(0, 0, 0, 0, 1, 0, color='brown')
-ax.quiver(0, 0, 0, 0, 0, 1, color='green')
+    ax.scatter(0, 0, 0, label='A', color='black', s=100)
+    ax.scatter(d_x, d_y, d_z, label='B', color='grey', s=100)
 
-# ax.quiver(d_x, d_y, d_z, enhets_vektorer_B[0,0], enhets_vektorer_B[1,0], enhets_vektorer_B[2,0], color='green')
-# ax.quiver(d_x, d_y, d_z, enhets_vektorer_B[0,1], enhets_vektorer_B[1,1], enhets_vektorer_B[2,1], color='green')
-# ax.quiver(d_x, d_y, d_z, enhets_vektorer_B[0,2], enhets_vektorer_B[1,2], enhets_vektorer_B[2,2], color='green')
+    ax.quiver(0, 0, 0, 1, 0, 0, color='orange')
+    ax.quiver(0, 0, 0, 0, 1, 0, color='brown')
+    ax.quiver(0, 0, 0, 0, 0, 1, color='green')
 
-ax.quiver(d_x, d_y, d_z, enhets_vektorer_B[0, 0], enhets_vektorer_B[1, 0], enhets_vektorer_B[2, 0], color='orange')
-ax.quiver(d_x, d_y, d_z, enhets_vektorer_B[0, 1], enhets_vektorer_B[1, 1], enhets_vektorer_B[2, 1], color='brown')
-ax.quiver(d_x, d_y, d_z, enhets_vektorer_B[0, 2], enhets_vektorer_B[1, 2], enhets_vektorer_B[2, 2], color='green')
+    ax.quiver(d_x, d_y, d_z, enhets_vektorer_B[0, 0], enhets_vektorer_B[1, 0], enhets_vektorer_B[2, 0], color='orange')
+    ax.quiver(d_x, d_y, d_z, enhets_vektorer_B[0, 1], enhets_vektorer_B[1, 1], enhets_vektorer_B[2, 1], color='brown')
+    ax.quiver(d_x, d_y, d_z, enhets_vektorer_B[0, 2], enhets_vektorer_B[1, 2], enhets_vektorer_B[2, 2], color='green')
 
-# ax.quiver(x_new, y_new, z_new, x_bar_new[0], x_bar_new[1], x_bar_new[2], color='red')
-# ax.quiver(x_new, y_new, z_new, y_bar_new[0], y_bar_new[1], y_bar_new[2], color='red')
-# ax.quiver(x_new, y_new, z_new, z_bar_new[0], z_bar_new[1], z_bar_new[2], color='red')
+    ax.quiver(0, 0, 0, x_A, y_A, z_A, color='red')
+    ax.quiver(0, 0, 0, d_x, d_y, d_z, color='blue')
+    ax.quiver(d_x, d_y, d_z, (x_A - d_x), (y_A - d_y), (z_A - d_z), color='magenta')
 
-ax.quiver(0, 0, 0, x_A, y_A, z_A, color='red')
-ax.quiver(0, 0, 0, d_x, d_y, d_z, color='blue')
-ax.quiver(d_x, d_y, d_z, (x_A - d_x), (y_A - d_y), (z_A - d_z), color='magenta')
+    ax.set_xlabel('x')
+    ax.set_ylabel('y')
+    ax.set_zlabel('z')
 
-ax.set_xlabel('x')
-ax.set_ylabel('y')
-ax.set_zlabel('z')
+    ax.legend()
+    plt.show()
 
-plt.show()
+    end_time(start)
