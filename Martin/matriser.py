ee7a74be48183186fce275dc9ff761a7d6526746
MartinStjernkvist <mr.martin.pro@gmail.com>
2025-01-02 21:56:58 +0100

    .

--- Martin/matriser.py
+++ Martin/matriser.py
@@ -8,7 +8,8 @@ from visualisera_bin_fil import fantom_matris, visualisera_matris
 # slicad_fantom_matris = fantom_matris[30:230, 40:210, 570:900]
 # slicad_fantom_matris = fantom_matris[50:-50, 50:200, 600:1100] # denna matris inkluderar benmärg i rygg, samt bröst
 # slicad_fantom_matris = fantom_matris[:, :, :]
-slicad_fantom_matris = fantom_matris[50:-50, 50:200, 600:1100]  # denna matris inkluderar benmärg i rygg, samt bröst
+# slicad_fantom_matris = fantom_matris[50:-50, 50:200, 600:1100]  # denna matris inkluderar benmärg i rygg, samt bröst
+slicad_fantom_matris = fantom_matris[50:-50, 50:200, 650:1050]  # samma som ovan, fast har endast med intervallet varvid 10**5 iterationer ger något utslag
 
 
 x, y, z = slicad_fantom_matris.shape
@@ -37,6 +38,9 @@ mask = np.isin(slicad_fantom_matris, target_values)
 slicad_benmärg_matris[mask] = slicad_fantom_matris[mask]
 
 if __name__ == "__main__":
+    visualisera = visualisera_matris(slicad_fantom_matris)
+    visualisera.show()
+
     visualisera = visualisera_matris(slicad_njure_matris)
     visualisera.show()
 
