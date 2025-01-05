c261c40e3aa667e8971d5ecf9ee5f30f43350e07
MartinStjernkvist <mr.martin.pro@gmail.com>
2025-01-05 22:18:39 +0100

    .

--- Martin/visualisera_resultat_ugp1.py
+++ Martin/visualisera_resultat_ugp1.py
@@ -21,4 +21,10 @@ if __name__ == "__main__":
     print(f'benmärg eV / decay: ', np.sum(benmärg_matris_deponerad_energi) / iterationer_tot)
     print(f'ryggrad benmärg eV / decay: ', np.sum(ryggrad_matris_deponerad) / iterationer_tot)
 
-    np.save('resultat_5_E6', ryggrad_matris_deponerad)
+    # np.save('resultat_5_E6', ryggrad_matris_deponerad)
+
+    resultat_5_E6 = np.load('resultat_5_E6.npy')
+    visualisera = visualisera_matris(resultat_5_E6, visa_något=True)
+    visualisera.show()
+
+    print(f'ryggrad benmärg eV / decay: ', np.sum(resultat_5_E6) / iterationer_tot)
