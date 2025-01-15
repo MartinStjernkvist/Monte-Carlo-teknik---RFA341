4eaafc83d2a052d0c318525a597eb9493cb769c9
MartinStjernkvist <mr.martin.pro@gmail.com>
2025-01-15 19:40:43 +0100

    .

--- Martin/upg2_laddad_partikel_väg_alfa.py
+++ Martin/upg2_laddad_partikel_väg_alfa.py
@@ -1,6 +1,9 @@
 from imports import *
 from upg2_energi_efter_förlust import energi_efter_energiförlust
 
+from upg12_steg_transformation import transformera_koordinatsystem
+from upg1_förflyttning import förflyttning
+
 
 def laddad_partikel_väg(energi_start, position_start, phi, theta, steglängd, radie_sfär, rho_medium,
                         stopping_power_data,
@@ -19,8 +22,55 @@ def laddad_partikel_väg(energi_start, position_start, phi, theta, steglängd, r
     :return: Energideponeringen innanför sfären.
     """
 
+    """
     # Initiera tomma listor för att spara datan.
-    x, y, z, dos = [], [], [], []
+    x_list, y_list, z_list, dos_list = [], [], [], []
+
+    position_vektor = position_start
+    x, y, z = position_vektor[0], position_vektor[1], position_vektor[2]
+
+    energi = energi_start
+
+    # Dela upp steglängden i mindre steg.
+    steg_storlek = steglängd / max_antal_steg
+
+    # Håll reda på antalet steg.
+    steg_tagna = 0
+
+    # Under tiden som partikeln fortfarande inte tagit hela sitt steg,
+    # samt att partikeln inte förlorat all sin energi.
+    while steg_tagna < max_antal_steg and energi > 0:
+
+        steg_tagna += 1
+
+        # Ta ett litet steg.
+        dx, dy, dz = transformera_koordinatsystem(steg_storlek, phi, theta, steg_storlek, 0, 0)
+        x, y, z, _, _, _ = förflyttning(x, y, z, dx, dy, dz)
+        position_vektor = np.array([x, y, z])
+
+        # Håll reda på ifall partikeln befinner sig i sfären eller inte.
+        # Ekvationen för en sfär: x^2 + y^2 + z^2 = r^2
+        if np.sqrt(np.dot(position_vektor, position_vektor)) > radie_sfär:
+            # print(f'Energideponering i position ', position_vektor)
+            break
+
+        else:
+            # Beräkna energin efter steget.
+            energi_ny = energi_efter_energiförlust(energi, steg_storlek, rho_medium, stopping_power_data)
+            # print(f'ny energi: {energi * 10 ** (-6):.2f} MeV')
+
+            # Spara mätpunkter.
+            dos_list.append(energi - energi_ny)
+            x_list.append(position_vektor[0])
+            y_list.append(position_vektor[1])
+            z_list.append(position_vektor[2])
+
+            energi = energi_ny
+            continue
+
+    """
+    # Initiera tomma listor för att spara datan.
+    x_list, y_list, z_list, dos_list = [], [], [], []
 
     position_vektor = position_start
     energi = energi_start
@@ -63,15 +113,16 @@ def laddad_partikel_väg(energi_start, position_start, phi, theta, steglängd, r
             # print(f'ny energi: {energi * 10 ** (-6):.2f} MeV')
 
             # Spara mätpunkter.
-            dos.append(energi - energi_ny)
-            x.append(position_vektor[0])
-            y.append(position_vektor[1])
-            z.append(position_vektor[2])
+            dos_list.append(energi - energi_ny)
+            x_list.append(position_vektor[0])
+            y_list.append(position_vektor[1])
+            z_list.append(position_vektor[2])
 
             energi = energi_ny
             continue
 
+
     # Beräkna den totala energideponeringen (i sfären) från partikeln.
     energideponering = energi_start - energi
     # print(f'energideponering: {energideponering} eV')
-    return energideponering, x, y, z, dos
+    return energideponering, x_list, y_list, z_list, dos_list
